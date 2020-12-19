04af070dfeea0c72d8367e36e17064794fa68a6d
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-12-19 09:00:08 +0100

    fiels names

--- models/calibre/Book.go
+++ models/calibre/Book.go
@@ -8,17 +8,17 @@ type Book struct {
 	ID           uint
 	Title        string
 	Sort         string
-	SeriesIndex  float64
-	AuthorSort   string
+	SeriesIndex  float64 `db:"series_index"`
+	AuthorSort   string  `db:"author_sort"`
 	Isbn         string
 	Lccn         string
 	Path         string
 	Flags        int
 	Uuid         string
-	HasCover     bool
+	HasCover     bool `db:"has_cover"`
 	Timestamp    time.Time
 	Pubdate      time.Time
-	LastModified time.Time
+	LastModified time.Time `db:"last_modified"`
 	Comment      string
 	Authors      []uint
 }
